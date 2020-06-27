63ce43d38dec7a6c015394d88f8d2c9722b9394e
Muhammad Ali <59179161+080HACKER@users.noreply.github.com>
2020-06-26 17:57:32 -0700

    Add files via upload

---
+++ Tool1/about.sh
@@ -0,0 +1,18 @@
+clear
+h="\033[32;1m"
+m="\033[31;1m"
+k="\033[33;1m"
+b="\033[34;1m"
+c="\033[35;1m"
+pu="\033[36;1m"
+p="\033[37;1m"
+
+echo ""
+echo ""
+echo ""
+echo ""
+             echo      "${m} TOOL ${p}VERSION ${p} 0.64"
+             echo      "${pu} CREATED ${h} DATE ${m} Fri, Jun 26 2020"
+             echo      "${p} TOOL OWNER HE WAS ${pu} ALI ${p}(${m}080HACKER${p})"
+             echo      "${h} WhatsApp ME ${p}234${h}9035145696"
+             echo      "${c} MY YOUTUBE ${m}CHANNEL IS${p} @${k} 080HACKERS"
